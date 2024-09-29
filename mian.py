3023b72fdb33847fd146bdb2792f4ca0d8250db0
nat5uu <150592608+nat5uu@users.noreply.github.com>
2024-09-29 12:26:22 +0200

    Create mian.py

---
+++ mian.py
@@ -0,0 +1,50 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import time
+
+url = 'website.org'  
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
+}
+
+
+
+def get_artical(url,terms):
+    r = requests.get(url, headers=headers)
+
+    # 200 is code for connected
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.content, 'html.parser')
+        articals = soup.find_all("span")
+        # print(articals)
+        for artical in articals:
+            content = artical.select_one("a")       
+            if not content == None:
+                if any(re.search(term, content.text, re.IGNORECASE) for term in terms):
+                    headline = content.text
+                    print(headline)
+    else:
+        print(f"Error: {r.status_code}")
+def get_next_site(url,next_site):
+    req = requests.get(url,headers=headers)
+    soup = BeautifulSoup(req.content,'html.parser')
+    element = soup.find(attrs={"aria-label":f"Seite {next_site}"})
+    return element["href"]
+
+if __name__ == "__main__":
+    pages = 28
+
+    # what are the keywords
+    terms = ['slide','closure', 'fastener']
+
+    for i in range(1,pages+1):
+        time.sleep(2)
+        print(f"Seite: {i}")
+        get_artical(url,terms)
+        if i<pages:
+            url = get_next_site(url,i+1)
+        
+        
+
