aae987c824eb6f6521667c249fbf978527204b36
nat5uu <150592608+nat5uu@users.noreply.github.com>
2024-09-29 16:09:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 url = 'website.org'  
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
+    'User-Agent': ''
 }
 
 
